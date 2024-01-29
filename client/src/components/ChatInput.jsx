d9a2908c04fda7f0ada8fbe50ee92bf92bd6cf3d
Ashish Lal <ashishlal2003@gmail.com>
2024-01-29 13:20:56 +0530

    chatinput

--- client/src/components/ChatInput.jsx
+++ client/src/components/ChatInput.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "../styles/chatInput.css";
 import Picker from "emoji-picker-react";
 import {IoMdSend} from 'react-icons/io';
+import {BsEmojiSmileFill} from 'react-icons/bs';
 
 function ChatInput() {
   return (
